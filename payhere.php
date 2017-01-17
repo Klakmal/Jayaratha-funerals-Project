8549a62b8a98e6b6bbf29537aebeef235393fc16
susith <susithgunathilaka@gmail.com>
2017-01-17 11:15:14 +0530

    paypal

--- payhere.php
+++ payhere.php
@@ -91,7 +91,7 @@ session_start(); ?>
                 </form>
                 </td>
                 <td>
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post"><!--test paypal API URL-->
+                <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post"><!--test paypal API URL-->
 
                   <!-- Identify your business so that you can collect the payments. -->
                   <input type="hidden" name="business" value="jayaratnefuneraldirectors@gmail.com"><!--Business email-->
